30362377309c4c690d16defaba68821dc1afe509
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-17 09:55:48 +0700

    node version

--- db.js
+++ db.js
@@ -1,13 +1,14 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-    host: 'localhost',
+    host: 'ec2-54-225-68-71.compute-1.amazonaws.com',
     port: 5432,
-    database: 'NODE1305',
-    user: 'postgres',
-    password: 'khoapham',
+    database: 'd4qrhr5sa3u7i9',
+    user: 'tdmlbrzpddgdep',
+    password: '684998fbbbe5f697ad876b385122ace24ad3bd0fd295dbe0ffb1a85561d644a1',
     max: 100,
-    idleTimeoutMillis: 1000
+    idleTimeoutMillis: 1000,
+    ssl: true
 });
 
 function queryDB(sql, arrValue) {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const HotGirl = require('./HotGirl');
+const HotGirl = require('./HotGirl');
 
 const app = express();
 app.listen(process.env.PORT || 3000, () => console.log('Server started'));
@@ -9,20 +9,20 @@ app.set('views', './views');
 
 app.get('/', (req, res) => res.render('a'));
 
-// app.get('/show/:id', async (req, res) => {
-//     const { id } = req.params; 
-//     const hotGirl = new HotGirl(id);
-//     try {
-//         const info = await hotGirl.getInfo();
-//         res.render('home', info);
-//     } catch (err) {
-//         res.send(err.toString());
-//     }
-// });
+app.get('/show/:id', async (req, res) => {
+    const { id } = req.params; 
+    const hotGirl = new HotGirl(id);
+    try {
+        const info = await hotGirl.getInfo();
+        res.render('home', info);
+    } catch (err) {
+        res.send(err.toString());
+    }
+});
 
-// app.get('/like/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const hotGirl = new HotGirl(id);
-//     const like = await hotGirl.hitLike();
-//     res.send(like + '');
-// });
+app.get('/like/:id', async (req, res) => {
+    const { id } = req.params;
+    const hotGirl = new HotGirl(id);
+    const like = await hotGirl.hitLike();
+    res.send(like + '');
+});
