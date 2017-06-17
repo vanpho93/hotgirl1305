772e57a5155c5a0158a67ce918648643e39033a8
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-17 09:00:50 +0700

    xong

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express');
-const HotGirl = require('./HotGirl');
+// const HotGirl = require('./HotGirl');
 
 const app = express();
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/show/:id', async (req, res) => {
-    const { id } = req.params; 
-    const hotGirl = new HotGirl(id);
-    try {
-        const info = await hotGirl.getInfo();
-        res.render('home', info);
-    } catch (err) {
-        res.send(err.toString());
-    }
-});
+app.get('/', (req, res) => res.render('a'));
 
-app.get('/like/:id', async (req, res) => {
-    const { id } = req.params;
-    const hotGirl = new HotGirl(id);
-    const like = await hotGirl.hitLike();
-    res.send(like + '');
-});
+// app.get('/show/:id', async (req, res) => {
+//     const { id } = req.params; 
+//     const hotGirl = new HotGirl(id);
+//     try {
+//         const info = await hotGirl.getInfo();
+//         res.render('home', info);
+//     } catch (err) {
+//         res.send(err.toString());
+//     }
+// });
+
+// app.get('/like/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const hotGirl = new HotGirl(id);
+//     const like = await hotGirl.hitLike();
+//     res.send(like + '');
+// });
